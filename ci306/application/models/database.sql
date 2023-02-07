f08e242314450b130af6a0dfed9f9384af593faa
Hery00 <handrianimanana@gmail.com>
2023-02-07 15:29:59 +0300

    update base

--- ci306/application/models/database.sql
+++ ci306/application/models/database.sql
@@ -1,2 +1,16 @@
 create database Takalo;
 use Takalo;
+
+
+
+create table user
+(
+    Id_user int primary key auto_increment,
+    Email varchar(50),
+    Pwd varchar(8),
+    Categorie int
+);
+
+
+
+
