87a5d38cf25b670a8873e5645e179d7387ff5dd4
Fanilo00 <120622693+Fanilo00@users.noreply.github.com>
2023-02-08 00:40:43 +0300

    modif base

--- ci306/application/models/database.sql
+++ ci306/application/models/database.sql
@@ -13,6 +13,7 @@ create table user
 );
 
 
+
 create table objet
 (
     Id_Objet int primary key auto_increment,
@@ -24,11 +25,27 @@ create table objet
     foreign key (Categorie) references Categorie(Categorie),
     foreign key (Id_user) references user(Id_User)
 );
+insert into objet values (null,1,'New Music Taste','CDAlbum.jpg',5,'Musique');
+insert into objet values (null,1,'Elegant Shoes','homme.jpg',100,'Chaussures');
+insert into objet values (null,1,'Diary For Your Best Handwriting','journal.jpg',10,'Accessoires');
+insert into objet values (null,2,'Autumn Flock High Heel Sandals','talon.jpg',120,'Chaussures');
+insert into objet values (null,2,'Sport shoes','tenis.jpg',50,'Chaussures');
+insert into objet values (null,3,'Summer Tendance','lunettes.jpg',25,'Accessoires');
+insert into objet values (null,3,'Performant Cellphone','phone.jpg',70,'technologie');
+
+
 
 create table Categorie
 (
     Categorie varchar(20) primary key 
 );
+insert into categorie values ('Musique');
+insert into categorie values ('Accessoires');
+insert into categorie values ('Chaussures');
+insert into categorie values ('Technologie');
+
+
+
 
 
 
