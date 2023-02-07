5b095e944a71a5bec3d7e217c28da0acb4eadb36
Bricecyril <120583961+Bricecyril@users.noreply.github.com>
2023-02-07 23:44:26 +0300

    Login

--- ci306/application/views/Page/Login.php
+++ ci306/application/views/Page/Login.php
@@ -1,42 +1,55 @@
-
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../Assets/Images/">
-    <link rel="stylesheet" href="../Assets/Css/Style1.css">
-    <link rel="stylesheet" href="../Assets/Bootstrap/bootstrap.css">
-    <title>Login</title>
-</head>
-<body>
-        <form action="<?php echo $_SERVER['SCRIPT_NAME']?>" method="POST">
-            <div class="left col-lg-4 col-md-4 col-sm-3 hidden-xs"></div>
-            <div id="formbox" class="col-lg-4 col-md-4 col-sm-3 hidden-xs">
-            <center><h2><p style="margin:30px">Welcome</p></h2></center>
-            <div class="form-group">
-            <input name="email" type="text" class="form-control" placeholder="Email">
-            <label for="text"></label>
-            <input name="mdp" type="password" class="form-control" placeholder="password">
-            <br>
-            <?php if(isset($_GET['error'])) { ?>
-        <?php if($_GET['error']==1) { ?>
-            <center><span class="label label-danger">Please,check your email address!</span></center>
-        <?php } ?>
-        <?php if($_GET['error']==2) { ?>
-            <center><span class="label label-danger">Wrong password!</span></center>
-        <?php } ?>
-        <?php if($_GET['error']==3) { ?>
-            <center><span class="label label-danger">Please retry!</span></center>
-        <?php } ?>
-    <?php } ?>
-            <center><button type="submit" name="btnlog" class="btn btn-default">LOGIN</button></center>
-        </br>
-        </form>
-       
-        <a href="Register.php"><div class="register">Register?</div></a>
-      
-        <div class="right col-lg-4 col-md-4 col-sm-3 hidden-xs"></div>          
-</body>
+  <head>
+  	<title>Login</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+	<link href="https://fonts.googleapis.com/css?family=Lato:300,400,700&display=swap" rel="stylesheet">
+
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+	
+	<link rel="stylesheet" href="<?php echo base_url();?>Assets/Loginstyle/css/style.css">
+
+	</head>
+	<body class="img js-fullheight" style="background-image: url(<?php echo base_url();?>Assets/Loginstyle/images/Back.jpg);">
+	<section class="ftco-section">
+		<div class="container">
+			<div class="row justify-content-center">
+				<div class="col-md-6 text-center mb-5">
+					<h2 class="heading-section">LOGIN</h2>
+				</div>
+			</div>
+			<div class="row justify-content-center">
+				<div class="col-md-6 col-lg-4">
+					<div class="login-wrap p-0">
+		      	<h3 class="mb-4 text-center">Have an account?</h3>
+		      	<form action="<?php echo base_url('LOGIN/log');?>" class="signin-form" method="post">
+		      		<div class="form-group">
+		      			<input type="text" class="form-control" placeholder="Username" required>
+		      		</div>
+	            <div class="form-group">
+	              <input id="password-field" type="password" class="form-control" placeholder="Password" required>
+	              <span toggle="#password-field" class="fa fa-fw fa-eye field-icon toggle-password"></span>
+	            </div>
+	            <div class="form-group">
+	            	<button type="submit" class="form-control btn btn-primary submit px-3">Sign In</button>
+	            </div>
+	            
+                </form>
+	          <div class="social d-flex text-center">
+	          	<a href="#" class="px-2 py-2 mr-md-1 rounded"><span class="ion-logo-facebook mr-2"></span>Sign Up</a>
+	          </div>
+		      </div>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<script src="<?php echo base_url();?>Assets/Loginstyle/js/jquery.min.js"></script>
+  <script src="<?php echo base_url();?>Assets/Loginstyle/js/popper.js"></script>
+  <script src="<?php echo base_url();?>Assets/Loginstyle/js/bootstrap.min.js"></script>
+  <script src="<?php echo base_url();?>Assets/Loginstyle/js/main.js"></script>
+
+	</body>
 </html>
