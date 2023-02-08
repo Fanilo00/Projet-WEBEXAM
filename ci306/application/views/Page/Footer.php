12c7ffc5a2cb4281070d1ccf10242839a151ef3c
Bricecyril <120583961+Bricecyril@users.noreply.github.com>
2023-02-08 09:02:31 +0300

    front and register

--- ci306/application/views/Page/Footer.php
+++ ci306/application/views/Page/Footer.php
@@ -1,5 +1,40 @@
+<footer class="footer-59391">
+      
+        <div class="container">
 
-
-
-</body>
-</html>
+          
+          <div class="row mb-5">
+            <div class="col-md-4">
+              <div class="site-logo">
+                <a href="#">Colorlib</a>
+              </div>
+            </div>
+            <div class="col-md-8 text-md-right">
+              <ul class="list-unstyled social-icons">
+                <li><a href="#" class="fb"><span class="icon-facebook"></span></a></li>
+                <li><a href="#" class="tw"><span class="icon-twitter"></span></a></li>
+                <li><a href="#" class="in"><span class="icon-instagram"></span></a></li>
+                <li><a href="#" class="be"><span class="icon-behance"></span></a></li>
+                <li><a href="#" class="dr"><span class="icon-dribbble"></span></a></li>
+                <li><a href="#" class="yt"><span class="icon-play"></span></a></li>
+              </ul>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col ">
+              <div class="copyright">
+                <p><small>Copyright 2019. All Rights Reserved.</small></p>
+              </div>
+            </div>
+          </div>
+        
+      </div>
+    </footer>
+    <script src="<?php echo base_url();?>Assets/JS/footer/jquery-3.3.1.min.js"></script>
+    <script src="<?php echo base_url();?>Assets/JS/footer/popper.min.js"></script>
+    <script src="<?php echo base_url();?>Assets/JS/footer/bootstrap.min.js"></script>
+    <script src="<?php echo base_url();?>Assets/JS/header/jquery-3.3.1.min.js"></script>
+    <script src="<?php echo base_url();?>Assets/JS/header/popper.min.js"></script>
+    <script src="<?php echo base_url();?>Assets/JS/header/bootstrap.min.js"></script>
+    <script src="<?php echo base_url();?>Assets/JS/header/jquery.sticky.js"></script>
+    <script src="<?php echo base_url();?>Assets/JS/header/main.js"></script>
--- ci306/application/views/Page/Login.php
+++ ci306/application/views/Page/Login.php
@@ -33,7 +33,7 @@
 	              <span toggle="#password-field" class="fa fa-fw fa-eye field-icon toggle-password"></span>
 	            </div>
 	            <div class="form-group">
-	            	<button type="submit" class="form-control btn btn-primary submit px-3">Sign In</button>
+	            	<a href="<?php echo base_url('INSCRIT/forminscrit');?>"><button type="submit" class="form-control btn btn-primary submit px-3">Sign In</button></a>
 	            </div>
 	            
                 </form>
