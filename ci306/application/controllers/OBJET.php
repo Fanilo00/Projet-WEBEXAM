e17dafbbb55e890116996bae8995e3508eaeb5c7
Fanilo00 <120622693+Fanilo00@users.noreply.github.com>
2023-02-08 15:19:05 +0300

    Update OBJET.php

--- ci306/application/controllers/OBJET.php
+++ ci306/application/controllers/OBJET.php
@@ -17,8 +17,9 @@ class OBJET extends CI_Controller
         $data['Content']='Page/Home';
         $this->load->view('Page/Index',$data);
     }
-    public function getCategorie()
+    public function getCategorie($categorie)
     {
+        $categorie = $this->input->post('categorie');
         $this->load->model('MObjet');
         $data['Result']=$this->MObjet->get_objetuser();
         $data['Content']='Page/Categorie';
