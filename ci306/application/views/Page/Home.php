cc02237d3ab7785836accda7b2a98c724da317ab
Hery00 <handrianimanana@gmail.com>
2023-02-08 14:40:08 +0300

    css2

--- ci306/application/views/Page/Home.php
+++ ci306/application/views/Page/Home.php
@@ -1,20 +1,20 @@
 
 <?php for($i=0;$i<count($Objets);$i++) { ?>
 
-    <div class="col-sm-2">
-  <div class="card">
-    <div class="image">
-        
-      <img src="<?php echo base_url();?>Assets/Images/<?php echo $Objets[$i]['Photo'];?>"/>
+    <div class=" content col-sm-2">
+        <div class="card">
+            <div class="image">
+                
+            <img src="<?php echo base_url();?>Assets/Images/<?php echo $Objets[$i]['Photo'];?>"/>
+            </div>
+            <div class="card-inner">
+            <div class="header">
+            <p class="p1"><?php echo $Objets[$i]['Descri'];?></p>
+                <p class="p2"><?php echo $Objets[$i]['Prix_estimatif'];?></p>
+            </div>
+            <div class="content">
+            </div>
+            </div>
+        </div>
     </div>
-    <div class="card-inner">
-      <div class="header">
-       <p class="p1"><?php echo $Objets[$i]['Descri'];?></p>
-        <p class="p2"><?php echo $Objets[$i]['Prix_estimatif'];?></p>
-    </div>
-    <div class="content">
-    </div>
-      </div>
-  </div>
-</div>
 <?php } ?>
