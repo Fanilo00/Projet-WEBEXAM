19b162f7f39a9e184aa0f8c2b316bd687d7a219e
Fanilo00 <120622693+Fanilo00@users.noreply.github.com>
2023-02-08 10:56:02 +0300

    update MUser

--- ci306/application/models/MUser.php
+++ ci306/application/models/MUser.php
@@ -36,8 +36,16 @@
 
         public function inscri_user($nom,$email,$pass)
         {
-            $requete = "insert into user values(null,'$nom','$email','$pass',0)";
-            $query = $this->db->query($requete);
+            $requete = "insert into user values(null,'%s','%s','%s',0)";
+            $requete = sprintf($requete,$this->db->escape($nom),$this->db->escape($email),$this->db->escape($pass));
+            $this->db->escape($requete);
+            $line = $this->db->affected_row();
+            $conf = true;
+            if($line==0)
+            {
+                $conf = false;
+            }
+            return $conf;
         }
 
         public function inscri_admin($nom,$email,$pass)
